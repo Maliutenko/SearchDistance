3eca4a1cfeeb712a2e0f320971d66ebb9a662146
Anton <maluk.tony@gmail.com>
2016-11-10 14:51:09 +0200

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,20 +1,20 @@
-package mypackage;
+package com.maluk.tony;
 
-public class MyClass {
+public class SearchDistanceClass {
 	
 	static int[] arr = {0, 23, 10, 14, 5, 9, 7};
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		int start = 4; //change 'start' variable
 		int end = 6; //change 'end' variable
 	
 		System.out.println(path(start, end) + " km");	
 	}
+	
 	public static int path(int c, int d){
 		int distance = 0;
-		for(int i = c + 1; i < d+1; i++){
+		for(int i = c + 1; i < d + 1; i++){
 			distance = distance + arr[i];
 		}
 		return distance;
